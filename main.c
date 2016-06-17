9a34b4eef5aaa43a79ba472f002ec3261c7274f1
QuaeSeONE <quaeseone@mail.ru>
2016-06-18 00:21:15 +0600

    Output added, minor fixes

--- main.c
+++ main.c
@@ -2,7 +2,12 @@
 #include <math.h>
 
 int main() {
-	double a, b, c, d, x1, x2;
+	double a, b, c, x1, x2, returnvalue;
 	printf("\nInput a, b, c:\n");
 	scanf("%lf %lf %lf", &a, &b, &c);
+	returnvalue = compute(a, b, c, &x1, &x2);
+	if (returnvalue) 
+		printf("x1 = , x2 = ", &.1x1, &.1x2);
+	else
+		printf("No roots!");
 }
