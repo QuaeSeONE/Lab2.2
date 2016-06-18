8e629bece9fb6d534bc3053793baf15bd8874b13
Сергей <quaeseone@mail.ru>
2016-06-18 12:04:36 +0600

    Minor fixes

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <math.h>
 
-double compute(double, double, double, double *, double *);
+int compute(double, double, double, double *, double *);
 
 int main() {
-	double a, b, c, x1, x2, returnvalue;
+	double a, b, c, x1, x2;
+	int returnvalue;
 	printf("\nInput a, b, c:\n");
 	scanf("%lf %lf %lf", &a, &b, &c);
 	returnvalue = compute(a, b, c, &x1, &x2);
@@ -14,7 +15,7 @@ int main() {
 		printf("No roots!\n");
 }
 
-double compute(double a, double b, double c, double *x1, double *x2) {
+int compute(double a, double b, double c, double *x1, double *x2) {
 	double d = b * b - 4 * a * c;
 	if (d < 0)
 		return 0;
